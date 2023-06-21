9179aa19b7b05851df42277b073d30d5c1424ac6
Richard <richardx366@gmail.com>
2023-06-21 01:03:25 -0400

    add  route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   logIn,
+  refreshToken,
   signUp,
   updateAuthLevel,
   updatePassword,
@@ -11,10 +12,7 @@ const authRouter = Router();
 
 authRouter.post('/login', logIn);
 authRouter.post('/signUp', signUp);
-authRouter.post('/logout', (req, res) => {
-  res.clearCookie('token');
-  res.send('success');
-});
+authRouter.post('/refresh', refreshToken);
 authRouter.put('/updatePassword/:id', updatePassword);
 authRouter.put('/authLevel/:id', updateAuthLevel);
 authRouter.put('/:id', updateUser);
